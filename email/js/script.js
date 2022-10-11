59b85df937a8fe9f420308f19e70ed844cb2bada
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-11 18:53:59 +0200

    change let to const

--- email/js/script.js
+++ email/js/script.js
@@ -6,30 +6,36 @@
 
 // dichiaro elementi html
 // dadi computer
-let iaOne  = document.getElementById("ia-1");
-let iaTwo  = document.getElementById("ia-2");
-let iaThree= document.getElementById("ia-3");
-let iaFour = document.getElementById("ia-4");
-let iaFive = document.getElementById("ia-5");
-let iaSix  = document.getElementById("ia-6");
+const iaOne  = document.getElementById("ia-1");
+const iaTwo  = document.getElementById("ia-2");
+const iaThree= document.getElementById("ia-3");
+const iaFour = document.getElementById("ia-4");
+const iaFive = document.getElementById("ia-5");
+const iaSix  = document.getElementById("ia-6");
 console.log(iaOne, iaTwo);
 // dadi player
-let playerOne  = document.getElementById("player-1");    
-let playerTwo  = document.getElementById("player-2");     
-let playerThree  = document.getElementById("player-3");     
-let playerFour  = document.getElementById("player-4");     
-let playerFive  = document.getElementById("player-5");     
-let playerSix  = document.getElementById("player-6"); 
+const playerOne  = document.getElementById("player-1");    
+const playerTwo  = document.getElementById("player-2");     
+const playerThree  = document.getElementById("player-3");     
+const playerFour  = document.getElementById("player-4");     
+const playerFive  = document.getElementById("player-5");     
+const playerSix  = document.getElementById("player-6"); 
 
 console.log(playerFive,playerFour,playerOne,playerSix,playerTwo,playerThree);
 // operatori di relazione
-let equalTo  = document.getElementById("equal-to");
-let greaterThan = document.getElementById("greater-than");
-let lessthan = document.getElementById("less-than");
+const equalTo  = document.getElementById("equal-to");
+const greaterThan = document.getElementById("greater-than");
+const lessthan = document.getElementById("less-than");
+
+// bottoni
+const btnRoll = document.getElementById("roll");
+const btnRetry = document.getElementById("retry");
 
 // creiamo 2 randomizzatori
 
-// const aiDice = Math.floor((Math.random()*6)+1);
+let aiDice = Math.floor((Math.random()*6)+1);
+
+
 
 
 // const playerDice = Math.floor((Math.random()*6)+1);
